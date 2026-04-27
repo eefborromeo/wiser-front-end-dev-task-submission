5f68ef2ef431825f41e8e3a3833151e7e21b63b7
Eloisa Borromeo <27989510+eefborromeo@users.noreply.github.com>
2026-04-27 13:19:52 +0800

    feat: Add JobRow component with styles and job details rendering

--- app/components/JobRow/JobRow.tsx
+++ app/components/JobRow/JobRow.tsx
@@ -1,9 +1,21 @@
-import type { Job } from "@/data/mockJobs";
+import type { Job } from '@/data/mockJobs';
+import styles from './JobRow.module.scss';
+import Link from 'next/link';
 
 type Props = {
-  job: Job;
+	job: Job;
 };
 
 export function JobRow({ job }: Props) {
-  return <></>;
+	return (
+		<article className={styles.row}>
+			<div>
+				<h3 className={styles.title}>{job.title}</h3>
+				<span className={styles.team}> {job.team}</span>
+			</div>
+			<div className={styles.brand}>{job.brand}</div>
+			<p className={styles.location}>{job.location}</p>
+			<Link href={job.link}>View job</Link>
+		</article>
+	);
 }
