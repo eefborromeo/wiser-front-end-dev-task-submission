58cec5ee608b5bfbe8c2125d94deea57834f5a33
Eloisa Borromeo <27989510+eefborromeo@users.noreply.github.com>
2026-04-27 13:19:38 +0800

    feat: Implement JobListing component with styling and mock data

--- app/components/JobListing/JobListing.tsx
+++ app/components/JobListing/JobListing.tsx
@@ -1,3 +1,15 @@
+import styles from './JobListing.module.scss';
+import { mockJobs } from '@/data/mockJobs';
+import { JobRow } from '../JobRow/JobRow';
+
 export function JobListing() {
-  return <></>;
+	return (
+		<div className={styles.page}>
+			<section className={styles.container}>
+				{mockJobs.map(jobItem => (
+					<JobRow key={jobItem.id} job={jobItem} />
+				))}
+			</section>
+		</div>
+	);
 }
